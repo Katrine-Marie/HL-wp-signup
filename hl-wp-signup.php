ea4da4ee284bcf513eca1af73c3128aa663d0af6
Katrine-Marie <fjordscene@gmail.com>
2019-06-06 13:52:37 +0200

    Created file header

---
+++ hl-wp-signup.php
@@ -0,0 +1,11 @@
+<?php
+/**********
+* Plugin Name: Heyloyalty WP signup
+* Plugin URI: https://github.com/Katrine-Marie/HL-wp-signup
+* Description: Easily embed a Heyloyalty signup form into your posts/pages
+* Version: 1.0.0
+* Author: Katrine-Marie Burmeister
+* Author URI: https://fjordstudio.dk
+* License:     GNU General Public License v3.0
+* License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
