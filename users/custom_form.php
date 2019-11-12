7850077bcafcec8707ec2894e0abba8ecf6ae643
fjordscene@gmail.com <fjordscene@gmail.com>
2019-11-12 18:46:50 +0100

    Prepared Heyloyalty API conn

---
+++ users/custom_form.php
@@ -0,0 +1,16 @@
+<?php
+
+// ini_set('display_errors', 1);
+// error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT);
+
+require_once('../../../../wp-config.php');
+
+require '../HL-phpclient/src/HL-phpclient/HLCurlHandler.php';
+require '../HL-phpclient/src/HL-phpclient/HLBase.php';
+require '../HL-phpclient/src/HL-phpclient/HLClient.php';
+require '../HL-phpclient/src/HL-phpclient/HLMembers.php';
+require '../HL-phpclient/src/HL-phpclient/HLLists.php';
+
+use Phpclient\HLClient;
+use Phpclient\HLMembers;
+use Phpclient\HLLists;
