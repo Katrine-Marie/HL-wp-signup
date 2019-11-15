d43b29ddc89d91916ba84e9f16d73b3251457cdb
fjordscene@gmail.com <fjordscene@gmail.com>
2019-11-15 17:16:23 +0100

    Set up HTML skeleton for form page

--- users/custom_form.php
+++ users/custom_form.php
@@ -7,5 +7,45 @@ $dataOpts = $_GET['dataOpts'];
 
 $dataArray = explode(",",$dataOpts);
 
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Heyloyalty Signup</title>
+
+  <style>
+
+    label, input {
+      display:block;
+    }
+    form div + div {
+      margin-top:6px;
+    }
+
+    input {padding:4px;}
+
+    input[type="submit"]{
+      cursor:pointer;
+
+      display:inline-block;
+      margin-top:6px;
+      padding:6px 12px;
+      border:none;
+      border-radius:6px;
+      color:#fff;
+      background:#4a70d1;
+    }
+    input[type="submit"]:hover {
+      background:#6687dd;
+    }
+
+  </style>
+</head>
+<body>
+
+
+
+</body>
+</html>
