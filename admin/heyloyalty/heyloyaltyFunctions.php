965226426149dbd44d136acf5f183a4ff1186f7d
fjordscene@gmail.com <fjordscene@gmail.com>
2019-11-21 19:25:23 +0100

    Commented out error reporting

--- admin/heyloyalty/heyloyaltyFunctions.php
+++ admin/heyloyalty/heyloyaltyFunctions.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT);
+// ini_set('display_errors', 1);
+// error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT);
 
 require 'HL-phpclient/src/HL-phpclient/HLCurlHandler.php';
 require 'HL-phpclient/src/HL-phpclient/HLBase.php';
