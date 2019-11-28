54a74fa9f9e559ea09726903baa7da9c1580c081
fjordscene@gmail.com <fjordscene@gmail.com>
2019-11-28 18:06:03 +0100

    Finished the widget form method

--- admin/widget_class.php
+++ admin/widget_class.php
@@ -14,17 +14,28 @@ class HL_WP__Widget extends WP_Widget {
 
 	// The widget form
 	public function form( $instance ) {
-    // Set widget defaults
+		// Set widget defaults
   	$defaults = array(
   		'title'    => 'Sign up for our newsletter',
   		'text'     => ''
   	);
 
-		// Parse current settings with defaults
+  	// Parse current settings with defaults
   	extract( wp_parse_args( ( array ) $instance, $defaults ) ); ?>
 
+  	<?php // Widget Title ?>
+  	<p>
+  		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>">Widget Title</label>
+  		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+  	</p>
 
+  	<?php // Text Field ?>
+  	<p>
+  		<label for="<?php echo esc_attr( $this->get_field_id( 'text' ) ); ?>">Descriptive Text</label>
+  		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'text' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'text' ) ); ?>" type="text" value="<?php echo esc_attr( $text ); ?>" />
+  	</p>
 
+  <?php 
 	}
 
 }
