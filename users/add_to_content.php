1db0ea633cf792e2aebf6c430b8c2ff5c6c6cb5c
fjordscene@gmail.com <fjordscene@gmail.com>
2019-12-03 18:20:53 +0100

    Set up options

--- users/add_to_content.php
+++ users/add_to_content.php
@@ -12,15 +12,27 @@ class heyloyaltyAutomaticAddition {
 
 	public function heyloyaltyForm_the_content( $content )
 	{
+
 		if( is_single() ){
 			if(get_option('hl-wp-api-key') == '' && get_option('hl-wp-secret-key') == ''){
 				$formContent = stripslashes(get_option('hl-wp-embed'));
 			}else {
+				$dataOpts = '';
+				if(get_option('hl-wp-field-firstname') || get_option('hl-wp-field-lastname')){
+					$dataOpts .= 'name';
+				}
+				if(get_option('hl-wp-field-mobile')){
+					$dataOpts .= ',mobile';
+				}
 
-				$formContent = stripslashes(get_option('hl-wp-embed'));
+				if(get_option('hl-wp-form-heading')){
+					$dataOpts .= '&formHeading=' . get_option('hl-wp-form-heading');
+				}
+
+
 
 			}
-			
+
 			$content .= '<br>' . $formContent;
 		}
 		return $content;
